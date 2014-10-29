251c0345db6ca8eb29ab7ddbd01650e6a06e412b
Matthew Story <matt.story@handshake.com>
2014-10-28 21:21:38 -0400

    add quick and dirty memory profiling
    Signed-off-by: Matthew Story <matt.story@handshake.com>

--- _sleeper_agent_activation.c
+++ _sleeper_agent_activation.c
@@ -18,6 +18,24 @@ char * sleeper_agent_state(void)
      return rv;
 }
 
+char * sleeper_agent_memstate(void)
+{
+     char *rv = NULL;
+     PyGILState_STATE gstate;
+
+     gstate = PyGILState_Ensure();
+     rv = PyString_AsString(
+          PyObject_Call(
+               PyObject_GetAttrString(
+                    PyImport_ImportModule("sleeper_agent"),
+                    "_get_mem_info"),
+               Py_BuildValue("()"), NULL));
+     PyGILState_Release(gstate);
+
+     return rv;
+}
+
+
 /* Return sleeper_agent_state() as Python string, for testing and fun */
 static PyObject *
 sleeper_agent_state_pyobject(PyObject *self, PyObject *args)
@@ -25,9 +43,17 @@ sleeper_agent_state_pyobject(PyObject *self, PyObject *args)
      return Py_BuildValue("s", sleeper_agent_state());
 }
 
+static PyObject *
+sleeper_agent_memstate_pyobject(PyObject *self, PyObject *args)
+{
+     return Py_BuildValue("s", sleeper_agent_memstate());
+}
+
 static PyMethodDef SleeperAgentMethods[] = {
      {"sleeper_agent_state",  sleeper_agent_state_pyobject, METH_VARARGS,
       "Return the sleeper agent state as a Python string."},
+	 {"sleeper_agend_memstate", sleeper_agent_memstate_pyobject, METH_VARARGS,
+	  "Return the sleeper agent memory state as a Python string."},
      {NULL, NULL, 0, NULL}
 };
 
--- sleeper_agent.py
+++ sleeper_agent.py
@@ -1,5 +1,18 @@
 import sys, traceback
+import gc
+import collections
 import _sleeper_agent_activation
+
+def _sizeof_fmt(num):
+    '''Shamelessly taken from StackOverflow:
+
+       http://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
+    '''
+    for x in ['b','KB','MB','GB','TB']:
+        if num < 1024.0:
+            break
+        num /= 1024.0
+    return "%3.1f %s" % (num, x)
 
 def _get_state_info():
     """Return state of current process, as a string.
@@ -13,3 +26,17 @@ def _get_state_info():
         "### Thread {0}:\n{1}".format(
             thread_id, ''.join(traceback.format_stack(frame)))
         for thread_id, frame in sys._current_frames().items() )
+
+
+def _get_mem_info():
+    '''Return memory usage information'''
+    obj_map = collections.defaultdict(lambda: { 'count': 0, 'size': 0})
+    for obj in gc.get_objects():
+        obj_map[type(obj)]['count'] += 1
+        obj_map[type(obj)]['size'] += sys.getsizeof(obj)
+
+    return "\n".join(
+        "{size:>8} {count:>8} object{s} {type_:>50}".format(type_=type_, count=val['count'],
+                                                            s='' if val['count'] == 1 else 's',
+                                                            size=_sizeof_fmt(val['size']))
+        for type_, val in sorted(obj_map.iteritems(), key=lambda v: -v[1]['size']))
