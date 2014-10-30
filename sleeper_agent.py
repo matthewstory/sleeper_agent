235cd1ef9d580bb7b2704586c5353bb52652fe05
Matthew Story <matt.story@handshake.com>
2014-10-30 14:14:13 -0400

    add totals to bottom of report, and add another section for uncollectable objects
    Signed-off-by: Matthew Story <matt.story@handshake.com>

--- sleeper_agent.py
+++ sleeper_agent.py
@@ -29,18 +29,38 @@ def _get_state_info():
         for thread_id, frame in sys._current_frames().items() )
 
 
+def _report_lines(obj_map):
+    '''Return a report for an object map'''
+    return ("{size:>8} {count:>8} object{s} {type_}".format(
+                type_=type_, count=val['count'],
+                s='' if val['count'] == 1 else 's',
+                size=_sizeof_fmt(val['size']))
+            for type_, val in sorted(obj_map.iteritems(),
+                key=lambda v: -v[1]['size']))
+
 def _get_mem_info():
     '''Return memory usage information'''
-    obj_map = collections.defaultdict(lambda: { 'count': 0, 'size': 0})
-    for obj in gc.get_objects():
-        obj_map[type(obj)]['count'] += 1
-        obj_map[type(obj)]['size'] += sys.getsizeof(obj)
+    counts_dflt = lambda: { 'count': 0, 'size': 0}
+    totals = collections.defaultdict(counts_dflt)
+    obj_map = collections.defaultdict(counts_dflt)
+    garbage_map = collections.defaultdict(counts_dflt)
 
-    return "\n".join(itertools.chain(('### Memory Usage',),
-        ("{size:>8} {count:>8} object{s} {type_}".format(
-            type_=type_,
-            count=val['count'],
-            s='' if val['count'] == 1 else 's',
-            size=_sizeof_fmt(val['size']))
-        for type_, val in sorted(obj_map.iteritems(),
-                                 key=lambda v: -v[1]['size']))))
+    # harvest objects and garbage
+    for objs, map_, type_ in ((gc.get_objects(), obj_map, 'objects'),
+                              (gc.garbage, garbage_map, 'garbage')):
+        for obj in objs:
+            map_[type(obj)]['count'] += 1
+            totals[type_]['count'] += 1
+
+            size = sys.getsizeof(obj)
+            map_[type(obj)]['size'] += size
+            totals[type_]['size'] += size
+
+    # build the report
+    return "\n".join(itertools.chain(
+        ('### Memory Usage',),
+        _report_lines(obj_map),
+        _report_lines({ 'total': totals['objects'] }),
+        ('### Garbage',),
+        _report_lines(garbage_map),
+        _report_lines({ 'garbage': totals['garbage'] })))
